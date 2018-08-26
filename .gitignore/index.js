b873e21ed55905ef32aa906467fc9c197de1f4e1
MisterSnake57 <42701205+MisterSnake57@users.noreply.github.com>
2018-08-26 18:01:34 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function() {
+    bot.user.setGame("Command: *help");
+    console.log("Connectedç");
+});
+
+bot.login("NDcyNDE0MzQ5OTE5NjQ5Nzky.DmHCXw.v9s3rZ3Iki04_ZIC-6HIkGaUXKc");
+
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes: \, -*help");
+    }
+
+    if (message.content === "Salut"){
+        message.reply("Bien le bonjour. :)");
+        console.log("Commande Salut effectué");
+    }
+}); 
+
+bot.on('message', message =>{
+    let command = message.content.split(" ")[0];
+    const args = message.content.slice(prefix.length).split(/ +/);
+    command = args.shift().toLowerCase();
+
+    if(command === "kick") {
+        let modRole = message.guild.roles.find("Kick");
+        if(!message.member.roles.has(modRole.id)) {
+            return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
+        }
+        if(message.mentions.users.size === 0) {
+            return message.reply("Merci de mentionner l'utilisateur à expluser.").catch(console.error);
+        }
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if(!kickMember) {
+            return message.reply("Cet utilisateur est introuvable ou impossible à expluser.")
+        }
+        if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
+            return message.reoply("Je n'ai pas la permission KICK_MEMBERSpour faire ceci.").catch(console.error);
+        }
+        kickMember.kick().then(message => {
+            message.reply(`${member.user.username} a été explusé avec succès.').catch(console.error`);
+        }).catch(console.error)
+    }
+
+});
