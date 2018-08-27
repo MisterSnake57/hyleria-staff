e17706f61677cf32f21c5cb055ae4406405517fe
MisterSnake57 <42701205+MisterSnake57@users.noreply.github.com>
2018-08-27 13:29:53 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -27,8 +27,8 @@ bot.on('message', message =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     command = args.shift().toLowerCase();
 
-    if(command === "KICK_MEMBER") {
-        let modRole = message.guild.roles.find("name", "Kick");
+    if(command === "kick") {
+        let modRole = message.guild.roles.find("name", "KICK_MEMBERS");
         if(!message.member.roles.has(modRole.id)) {
             return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
         }
